24cf55a0620f556e1c84cb8357a63bd48f86227b
Patrick Nelsen <55818910+nelsen129@users.noreply.github.com>
2023-06-24 15:53:36 -0700

    feat: add base files for frontend component

---
+++ infrastructure/components/frontend/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket         = "cloud-resume-challenge-state-bucket"
+    dynamodb_table = "cloud-resume-challenge-dynamodb-lock-state"
+    region         = "us-east-1"
+  }
+}
