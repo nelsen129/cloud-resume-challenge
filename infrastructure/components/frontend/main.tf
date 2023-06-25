536099799bd703c366261554fb222b1d50bc694c
Patrick Nelsen <55818910+nelsen129@users.noreply.github.com>
2023-06-25 06:51:52 -0700

    fix: add s3 policy to allow cloudfront to access s3 bucket

--- infrastructure/components/frontend/main.tf
+++ infrastructure/components/frontend/main.tf
@@ -81,3 +81,27 @@ module "cloudfront" {
     query_string           = true
   }
 }
+
+data "aws_iam_policy_document" "s3_policy" {
+  # Origin Access Controls
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${module.s3_bucket.s3_bucket_arn}/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = [module.cloudfront.cloudfront_distribution_arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = module.s3_bucket.s3_bucket_id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
