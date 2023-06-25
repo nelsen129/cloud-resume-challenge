d7c148395933e9c2f01a0faf1c05e69b5b5df0e5
Patrick Nelsen <55818910+nelsen129@users.noreply.github.com>
2023-06-25 08:59:07 -0700

    fix: move website files back to root of s3 bucket

--- infrastructure/components/frontend/main.tf
+++ infrastructure/components/frontend/main.tf
@@ -149,7 +149,7 @@ data "aws_iam_policy_document" "s3_policy" {
   # Origin Access Controls
   statement {
     actions   = ["s3:GetObject"]
-    resources = ["${module.s3_bucket.s3_bucket_arn}/static/*"]
+    resources = ["${module.s3_bucket.s3_bucket_arn}/*"]
 
     principals {
       type        = "Service"
