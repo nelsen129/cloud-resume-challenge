12c957b9c36e728ae03cda397104f21972132ca6
Patrick Nelsen <55818910+nelsen129@users.noreply.github.com>
2023-07-04 06:41:47 -0700

    feat: upload built lambda functions to s3 bucket

--- infrastructure/components/backend/main.tf
+++ infrastructure/components/backend/main.tf
@@ -25,3 +25,41 @@ module "dynamodb_table" {
 
   tags = var.tags
 }
+
+# tfsec:ignore:aws-s3-enable-bucket-logging
+module "lambda_build_s3_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 3.14"
+
+  bucket = trim(substr("lambda-builds-${var.name}-${var.environment}-bucket-${random_pet.this.id}", 0, 63), "-")
+
+  force_destroy = var.force_destroy
+
+  versioning = {
+    status     = true
+    mfa_delete = false
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "aws:kms"
+      }
+    }
+    bucket_key_enabled = true
+  }
+
+  tags = var.tags
+}
+
+resource "aws_s3_object" "lambda_build" {
+  for_each = fileset("${path.module}/../../../backend/out", "*.zip")
+
+  bucket = module.lambda_build_s3_bucket.s3_bucket_id
+  key    = each.key
+  source = "${path.module}/../../../backend/out/${each.key}"
+
+  source_hash = filemd5("${path.module}/../../../backend/out/${each.key}")
+
+  tags = var.tags
+}
