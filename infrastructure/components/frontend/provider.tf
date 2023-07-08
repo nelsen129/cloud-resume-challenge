611a339dd59eeaec03710bb185b4228e4dd9a38f
Patrick Nelsen <55818910+nelsen129@users.noreply.github.com>
2023-07-08 14:14:41 -0700

    fix: add us-east-1 provider back to frontend

--- infrastructure/components/frontend/provider.tf
+++ infrastructure/components/frontend/provider.tf
@@ -13,3 +13,21 @@ provider "aws" {
     role_arn = var.role_arn
   }
 }
+
+# Needed to delete current resources. Should be deleted next release after v0.1.0
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      component   = "frontend"
+      environment = var.environment
+      project     = var.name
+    }
+  }
+
+  assume_role {
+    role_arn = var.role_arn
+  }
+}
