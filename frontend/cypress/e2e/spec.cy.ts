0d1fdf7555e3f17fd26df56492a97e44627e6439
Patrick Nelsen <55818910+nelsen129@users.noreply.github.com>
2023-07-11 21:00:32 -0700

    feat: cypress test view counter

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,5 +1,22 @@
 describe("View Counter", () => {
-  it("Visits the about page", () => {
+  it("Updates the view counter on refresh", () => {
     cy.visit("/");
+
+    cy.wait(3000);
+
+    cy.get('[data-cy="view-count"]').then(($span) => {
+      const viewcount1 = parseFloat($span.text());
+
+      cy.reload().then(() => {
+        cy.wait(3000).then(() => {
+          cy.get('[data-cy="view-count"]').then(($span2) => {
+            const viewcount2 = parseFloat($span2.text());
+
+            // Make sure viewcount incremented
+            expect(viewcount2).to.eq(viewcount1 + 1);
+          });
+        });
+      });
+    });
   });
 });
