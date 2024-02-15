75c43f13683b6435279a5b483f8ace18cfc829e4
Patrick Nelsen <55818910+nelsen129@users.noreply.github.com>
2024-02-14 22:00:05 -0700

    55 add integration tests with cypress (#60)
    Signed-off-by: Patrick Nelsen <55818910+nelsen129@users.noreply.github.com>

---
+++ frontend/cypress/e2e/spec.cy.ts
@@ -0,0 +1,22 @@
+describe("View Counter", () => {
+  it("Updates the view counter on refresh", () => {
+    cy.visit("/");
+
+    cy.wait(3000);
+
+    cy.get('[data-cy="view-count"]').then(($span) => {
+      const viewcount1 = parseFloat($span.text());
+
+      cy.reload().then(() => {
+        cy.wait(3000).then(() => {
+          cy.get('[data-cy="view-count"]').then(($span2) => {
+            const viewcount2 = parseFloat($span2.text());
+
+            // Make sure viewcount incremented
+            expect(viewcount2).to.be.at.least(viewcount1 + 1);
+          });
+        });
+      });
+    });
+  });
+});
